850e9482ba31e83fae33b9a5a79c0b08b67e60b1
Felip De Torres <detorresphilip6@gmail.com>
2025-10-25 10:13:17 +0800

    Update Tailwind CSS content paths

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx, ts, tsx, vue, php, html}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  
   theme: {
     extend: {},
   },
